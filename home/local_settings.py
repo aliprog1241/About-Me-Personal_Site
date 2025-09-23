6b9dabe63e0f9b0c614bb66bc09f6829f1f5304a
Ali Miri <miri03369@gmail.com>
2025-09-23 19:17:32 +0330

    edit settings

---
+++ home/local_settings.py
@@ -0,0 +1,11 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-tq-c&^z9pa-!ki7rh@7_chn0c$iay=dk^2pp!%gule7#0c%tc+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DB_NAME = 'AboutMe'
+DB_USER = "postgres"
+DB_PASSWORD = 1234
+DB_HOST = "localhost"
+DB_PORT = 5432
