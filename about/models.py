3f9b8a64cd1ef81f79df3773cfeafd22bea9c6eb
Ali Miri <miri03369@gmail.com>
2025-09-28 13:33:50 +0330

    create new models for gallery

--- about/models.py
+++ about/models.py
@@ -26,3 +26,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to="portfolio/")
+    link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
